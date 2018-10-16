ffef4c61a0361ff8865431ad9ecd9951dd49803c
Adrian Mai <quocanh010@gmail.com>
2018-10-16 10:45:32 -0700

    Cheetah appread

--- HW1.m
+++ HW1.m
@@ -4,7 +4,7 @@ Background = S.TrainsampleDCT_BG
 
 %Calculating Piror Problability 
 Prior_Cheetah = size(Cheetah, 1) / (size(Cheetah,1) + size(Background, 1))
-Prior_Backgtound = size(Background, 1) / (size(Cheetah,1) + size(Background, 1))
+Prior_Background = size(Background, 1) / (size(Cheetah,1) + size(Background, 1))
 
 %Find the second largest DCT index
 [Cheetah_2nd, I_Cheetah] = max(abs(Cheetah(:,2:end)), [], 2)
@@ -14,8 +14,9 @@ I_Cheetah = I_Cheetah + 1
 I_Backgrough = I_Backgrough + 1
 % Draw histogram of the index
 P_X_g_Y_Cheetah = histogram(I_Cheetah,'NumBins', 63, 'BinLimits', [2 64], 'Normalization','probability')
+C_Cheetah =P_X_g_Y_Cheetah.Values
 P_X_g_Y_Background = histogram(I_Backgrough,'NumBins', 63, 'BinLimits', [2 64], 'Normalization','probability')
-
+C_Background = P_X_g_Y_Background.Values
 
 % Calculating P_Y_g_X
 img = imread('cheetah.bmp')
@@ -28,7 +29,20 @@ formatSpec = '%i'
 index_v = fscanf(fileID,formatSpec)
 index_v = int16(index_v)
 index_v = index_v + 1
-%compute dct
+%Padding to teh image
+B = padarray(img, [7 7], 'symmetric','post')
+%compute ans sliding window
+%  for i = 1:size(img, 1) 
+%      for j = 1:size(img, 2) 
+%          result_v(count) = zig_zag_v(B(i:i+7, j:j+7), index_v, C_Cheetah, C_Background, Prior_Cheetah, Prior_Background)
+%          count = count + 1
+%      end
+%  end
+result = nlfilter(  B, [8 8], @zig_zag_v)
+Cheetah_m = C_Cheetah(result) .* Prior_Cheetah
+Background_m = C_Background(result) .* Prior_Background
 
-
-w = [8,8]
+n_img = Background_m <= Cheetah_m
+imagesc(n_img)
+colormap(gray(256))
+axis equal
--- zig_zag_v.m
+++ zig_zag_v.m
@@ -1,8 +1,13 @@
 
 
 %compute dct
-function f = zig_zag_v(x, index_v)
-    f = x.'
-    f = f(:)
-    f(:) = f(index_v)
+function index_v = zig_zag_v(x)
+    x_dct = dct2(x)
+    ind = reshape(1:numel(x_dct), size(x_dct)); %# indices of elements
+    ind = fliplr( spdiags( fliplr(ind) ) );     %# get the anti-diagonals
+    ind(:,1:2:end) = flipud( ind(:,1:2:end) );  %# reverse order of odd columns
+    ind(ind==0) = [];                           %# keep non-zero indices
+    v = x_dct(ind)
+    [value, index_v] = max(abs(v(2:end)))
+    index_v = index_v + 1
 end
